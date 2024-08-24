613777e26306fad2790cdc75ed62be14fbd8de2d
Brother-x86 <heyler.quentin@gmail.com>
2024-08-24 13:53:30 +0200

    format code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #![windows_subsystem = "windows"]
-use std::{thread, time};
-use std::process::id; 
+use std::process::id;
 use std::process::Command;
-use sysinfo::{System, Pid};
+use std::{thread, time};
+use sysinfo::{Pid, System};
 
 use std::env;
 
@@ -12,33 +12,31 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let mut target_pid =  if args.len() > 1 {
+    let mut target_pid = if args.len() > 1 {
         let pid = args[1].parse::<u32>().unwrap();
         let args_pid = Pid::from_u32(pid);
         args_pid
-
-    }else {
+    } else {
         let parent_pid = sys.process(Pid::from_u32(id())).unwrap().parent().unwrap();
         parent_pid
     };
-    
-    let target_process= sys.process(target_pid).unwrap();
-    let target_path= target_process.exe().unwrap();
+
+    let target_process = sys.process(target_pid).unwrap();
+    let target_path = target_process.exe().unwrap();
 
     let mut sys = System::new_all();
-loop {
-    sys.refresh_all();
+    loop {
+        sys.refresh_all();
 
-    target_pid = match sys.process(target_pid) {
-        Some(_) => target_pid,
-        None    => {
-            let spawn = Command::new(target_path).spawn().unwrap();
-            Pid::from_u32(spawn.id())  
-        }
-    };
+        target_pid = match sys.process(target_pid) {
+            Some(_) => target_pid,
+            None => {
+                let spawn = Command::new(target_path).spawn().unwrap();
+                Pid::from_u32(spawn.id())
+            }
+        };
 
-    let sleep_time: time::Duration = time::Duration::from_millis(1000);
-    thread::sleep(sleep_time);
-
+        let sleep_time: time::Duration = time::Duration::from_millis(1000);
+        thread::sleep(sleep_time);
     }
 }
